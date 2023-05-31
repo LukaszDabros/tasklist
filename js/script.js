d9dcaaa8e1f69a78c825529f571fad1bd0810504
Lukasz Dabros <lukaszdabros@wp.pl>
2023-05-31 23:10:49 +0200

    adding new tasks and showinh if a task is done

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@
             htmlString += `
             <li
             ${task.done ? " style=\"text-decoration: line-through\"" : ""
-            }
+                }
             >
             ${task.content}
             </li>
@@ -28,8 +28,34 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+           
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+     
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
